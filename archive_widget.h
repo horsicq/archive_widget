20f650ae8b6129b2a13d14e4d72e547dcc2f9370
horsicq <horsicq@gmail.com>
2021-09-03 21:46:21 +0200

    Fix: 2021-09-03

--- archive_widget.h
+++ archive_widget.h
@@ -62,6 +62,7 @@ class Archive_widget : public XShortcutsWidget
 
 public:
     explicit Archive_widget(QWidget *pParent=nullptr);
+    // TODO setOptions
     void setFileName(QString sFileName,FW_DEF::OPTIONS options,QSet<XBinary::FT> stAvailableOpenFileTypes,QWidget *pParent=nullptr); // TODO options for Viewers TODO Device
     void setDirectoryName(QString sDirectoryName,FW_DEF::OPTIONS options,QSet<XBinary::FT> stAvailableOpenFileTypes,QWidget *pParent=nullptr);
     void setData(CreateViewModelProcess::TYPE type,QString sName,FW_DEF::OPTIONS options,QSet<XBinary::FT> stAvailableOpenFileTypes,QWidget *pParent=nullptr);
