f9ca1f875f51aa6b6d1cb2e9a84fe6aca875c2bb
horsicq <horsicq@gmail.com>
2021-10-27 20:53:12 +0200

    Fix: 2021-10-27

--- archive_widget.cmake
+++ archive_widget.cmake
@@ -2,10 +2,12 @@ include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 include(${CMAKE_CURRENT_LIST_DIR}/../FormatWidgets/allformatwidgets.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../XArchive/xarchives.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../XShortcuts/xshortcuts.cmake)
 
 set(ARCHIVE_WIDGET_SOURCES
     ${ALLFORMATWIDGETS_SOURCES}
     ${XARCHIVES_SOURCES}
+    ${XSHORTCUTS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/archive_widget.cpp
     ${CMAKE_CURRENT_LIST_DIR}/archive_widget.h
     ${CMAKE_CURRENT_LIST_DIR}/archive_widget.ui
