c59fbccc65125e6eda48f174631e73d4d6a937a8
horsicq <horsicq@gmail.com>
2022-10-15 00:45:55 +0200

    Fix: 2022-10-15

--- unpackfileprocess.cpp
+++ unpackfileprocess.cpp
@@ -39,16 +39,14 @@ void UnpackFileProcess::process()
     QElapsedTimer scanTimer;
     scanTimer.start();
 
-    g_pPdStruct->pdRecordOpt.bIsValid=true;
+    qint32 _nFreeIndex=XBinary::getFreeIndex(g_pPdStruct);
+    XBinary::setPdStructInit(g_pPdStruct,_nFreeIndex,0);
 
     bool bResult=XArchives::decompressToFile(g_sFileName,g_pRecord,sResultFileName,g_pPdStruct); // TODO Errors !!!
 
-    if(!(g_pPdStruct->bIsStop))
-    {
-        g_pPdStruct->pdRecordOpt.bSuccess=bResult;
-    }
+    g_pPdStruct->bIsStop=!(bResult);
 
-    g_pPdStruct->pdRecordOpt.bFinished=true;
+    XBinary::setPdStructFinished(g_pPdStruct,_nFreeIndex);
 
     emit completed(scanTimer.elapsed());
 }
