534a9574de32be392d70beb5a8c7568cbb2a345e
horsicq <horsicq@gmail.com>
2023-05-26 19:25:00 +0200

    Fix: 2023-05-26

--- dialogarchive.h
+++ dialogarchive.h
@@ -39,7 +39,7 @@ public:
 
     void setFileName(const QString &sFileName, FW_DEF::OPTIONS options, QSet<XBinary::FT> stAvailableFileTypes);
     void setDevice(QIODevice *pDevice, FW_DEF::OPTIONS options, QSet<XBinary::FT> stAvailableFileTypes);
-    void setDirectory(QString sDirectoryName, FW_DEF::OPTIONS options, QSet<XBinary::FT> stAvailableFileTypes);
+    void setDirectory(const QString &sDirectoryName, FW_DEF::OPTIONS options, QSet<XBinary::FT> stAvailableFileTypes);
     void setGlobal(XShortcuts *pShortcuts, XOptions *pXOptions);
     QString getCurrentRecordFileName();
 
