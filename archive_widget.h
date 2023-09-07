289ba6b5507463a5255e531a21e61299dd870d43
horsicq <horsicq@gmail.com>
2023-09-07 14:24:15 +0200

    Fix: 2023-09-07

--- archive_widget.h
+++ archive_widget.h
@@ -68,7 +68,7 @@ public:
     void setFileName(const QString &sFileName, const FW_DEF::OPTIONS &options,
                      const QSet<XBinary::FT> &stAvailableOpenFileTypes);  // TODO options for Viewers TODO Device
     void setDirectoryName(const QString &sDirectoryName, const FW_DEF::OPTIONS &options, const QSet<XBinary::FT> &stAvailableOpenFileTypes);
-    void setData(CreateViewModelProcess::TYPE type, const QString &sName, const FW_DEF::OPTIONS &options, QSet<XBinary::FT> stAvailableOpenFileTypes);
+    void setData(CreateViewModelProcess::TYPE type, const QString &sName, const FW_DEF::OPTIONS &options, const QSet<XBinary::FT> &stAvailableOpenFileTypes);
     QString getCurrentRecordFileName();
     QList<CreateViewModelProcess::RECORD> getRecordsByFileType(XBinary::FT fileType);
 
